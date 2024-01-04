f4a4dd6a3d955b0678e0884bda1db840dfa99205
Prashanth Gujjeti <pgujjeti@gmail.com>
2024-01-03 20:54:30 -0800

    added rollup/typescript to get build to work

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import commonjs from '@rollup/plugin-commonjs';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import json from '@rollup/plugin-json';
+import typescript from '@rollup/plugin-typescript';
 import css from 'rollup-plugin-lit-css';
 import { terser } from "rollup-plugin-terser";
 import nodePolyfills from 'rollup-plugin-node-polyfills';
@@ -13,7 +14,8 @@ export default {
     plugins: [terser()]
   },
   plugins: [
-    nodeResolve(),
+    nodeResolve({
+    }),
     commonjs(),
     css({ include: ['**/*.css', '**/*.scss'] }),
     json(),
@@ -21,6 +23,7 @@ export default {
       // This will create an empty tty module
       include: ['tty'],
       empty: ['tty']
-    })
+    }),
+    typescript(),
   ]
 };
