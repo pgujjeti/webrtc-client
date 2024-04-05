ff2b622fbdeb0cbaa1d09e4188bc243bea5b30c9
Prashanth Gujjeti <pgujjeti@gmail.com>
2024-04-04 18:49:49 -0700

    change start port

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './webrtc-widget/webrtc-widget'
 function App() {
   return (
     <div className="App">
-      <webrtc-widget caller="c3" callerName="c3 Major" password="cCaas95" sipRealm="pg2.sip.jambonz.cloud" wssUrl="wss://pg2.sip.jambonz.cloud:8443">
+      <webrtc-widget caller="c3" callerName="c3 Major" password="cCaas95" sipRealm="pgjbz2.sip.jambonz.cloud" wssUrl="wss://pgjbz2.sip.jambonz.cloud:8443">
     </webrtc-widget>
   </div>
   );
